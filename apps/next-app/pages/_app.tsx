7123cb9eca1284aeb0d9a6135282ca1eaf7b9af6
tommy-typos <137985975+tommy-typos@users.noreply.github.com>
2023-07-28 10:45:25 +0400

    added github corner

--- apps/next-app/pages/_app.tsx
+++ apps/next-app/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './globals.css';
+import GithubCorner from 'react-github-corner';
+import { twColors } from '../utils/twTheme';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,13 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Nx Visualize</title>
       </Head>
       <main className="flex h-full w-full">
+        <GithubCorner
+          direction="left"
+          bannerColor={twColors.fuchsia[950]}
+          svgStyle={{ mixBlendMode: 'darken' }}
+          size={100}
+          href="https://github.com/tommy-typos/nx-visualize-app"
+        />
         <Component {...pageProps} />
       </main>
     </>
